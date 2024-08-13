a29beebe1327101c98c7603ca5185873041b12ae
Aleksey228-pups <Aleksejsoronov3@gmail.com>
2024-08-13 19:36:08 +0300

    12/08

---
+++ food.py
@@ -0,0 +1,14 @@
+# Food = ["gold","zero","smail"]
+# Food.append(True)
+# print(Food)
+# Food.extend(["godzila", 2])
+# print(Food)
+# Food.remove('gold')
+# print(Food)
+# print("zero" not in Food)
+tuple_ = 1, 2, 3, 4
+tuple_2 = (1, 2, 3, 4)
+tuple_3 = tuple([1,2,3,4])
+print(tuple_)
+print(tuple_2)
+print(tuple_3)
